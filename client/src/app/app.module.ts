287496f0d6174456a221157bcb211ae5e64624b2
Joey Vinyard <jviny98@yahoo.com>
2018-02-07 14:29:54 -0500

    Generated initial components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { MapComponent } from './map/map.component';
+import { ListComponent } from './list/list.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    MapComponent,
+    ListComponent,
+    SettingsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
