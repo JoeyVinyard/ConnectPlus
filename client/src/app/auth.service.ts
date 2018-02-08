d43d8f7eeac05469d7f2604028ebd0f042478aea
Joey Vinyard <jviny98@yahoo.com>
2018-02-08 16:43:32 -0500

    Added foundation css to angular json

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Component } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+// import { Injectable } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import * as firebase from 'firebase/app';
 
 
-@Injectable()
-export class AuthService {
+// @Injectable()
+// export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { 
+//   constructor(public afAuth: AngularFireAuth) { 
 
-  }
-  login(){
-  	this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());//Creates popup, will be replaced later
-  }
-  logout(){
-  	this.af.Auth.auth.signOut();
-  }
+//   }
+//   login(){
+//   	this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());//Creates popup, will be replaced later
+//   }
+//   logout(){
+//   	this.af.Auth.auth.signOut();
+//   }
 
-}
+// }
