330ba1768e5a1230d17e13f166d67b5b5039c8ef
Cameron Glass <glass10@purdue.edu>
2018-02-11 15:33:48 -0500

    Large Splashpage Additions

--- client/src/app/splash/splash.component.ts
+++ client/src/app/splash/splash.component.ts
@@ -27,18 +27,115 @@ export class SplashComponent implements OnInit {
 		};
 
 		this.myParams = {
-			particles: {
-				number: {
-					value: 200,
+			"particles": {
+			  "number": {
+				"value": 202,
+				"density": {
+				  "enable": true,
+				  "value_area": 721.5354273894853
+				}
+			  },
+			  "color": {
+				"value": "#ffffff"
+			  },
+			  "shape": {
+				"type": "circle",
+				"stroke": {
+				  "width": 0,
+				  "color": "#000000"
 				},
-				color: {
-					value: '#ff0000'
+				"polygon": {
+				  "nb_sides": 5
 				},
-				shape: {
-					type: 'triangle',
+				"image": {
+				  "src": "img/github.svg",
+				  "width": 100,
+				  "height": 100
+				}
+			  },
+			  "opacity": {
+				"value": 0.5,
+				"random": false,
+				"anim": {
+				  "enable": false,
+				  "speed": 1,
+				  "opacity_min": 0.1,
+				  "sync": false
+				}
+			  },
+			  "size": {
+				"value": 3,
+				"random": true,
+				"anim": {
+				  "enable": false,
+				  "speed": 40,
+				  "size_min": 0.1,
+				  "sync": false
+				}
+			  },
+			  "line_linked": {
+				"enable": true,
+				"distance": 150,
+				"color": "#ffffff",
+				"opacity": 0.4,
+				"width": 1
+			  },
+			  "move": {
+				"enable": true,
+				"speed": 3,
+				"direction": "none",
+				"random": false,
+				"straight": false,
+				"out_mode": "out",
+				"bounce": false,
+				"attract": {
+				  "enable": false,
+				  "rotateX": 600,
+				  "rotateY": 1200
+				}
+			  }
+			},
+			"interactivity": {
+			  "detect_on": "canvas",
+			  "events": {
+				"onhover": {
+				  "enable": true,
+				  "mode": "bubble"
 				},
-			}
-		};
+				"onclick": {
+				  "enable": true,
+				  "mode": "repulse"
+				},
+				"resize": true
+			  },
+			  "modes": {
+				"grab": {
+				  "distance": 400,
+				  "line_linked": {
+					"opacity": 1
+				  }
+				},
+				"bubble": {
+				  "distance": 231.44200550588337,
+				  "size": 4.120772123013452,
+				  "duration": 2,
+				  "opacity": 1,
+				  "speed": 3
+				},
+				"repulse": {
+				  "distance": 100.84540486109416,
+				  "duration": 0.4
+				},
+				"push": {
+				  "particles_nb": 4
+				},
+				"remove": {
+				  "particles_nb": 2
+				}
+			  }
+			},
+			"retina_detect": true
+		  };
 	}
 
 }
