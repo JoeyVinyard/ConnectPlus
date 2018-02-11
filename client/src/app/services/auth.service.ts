79ab5b49dde558a3a8b44cd351e9d7af688493ab
niralirai <niralirai@yahoo.com>
2018-02-11 14:40:00 -0500

    signup and login return call

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -7,17 +7,19 @@ export class AuthService {
 		console.log(email, password);
 		//implement sign up using the createUserWithEmailAndPassword function in afAuth.auth
 		//Return the promise
-		return null;
+		return this.afAuth.auth.createUserWithEmailAndPassword(email,password);	
 	}
+
 	login(email, password): Promise<any>{
 		console.log(email, password);
 		//implement sign in using the signInWithEmailAndPassword function in afAuth.auth
 		//Return the promise
-		return null;
+		return this.afAuth.auth.signInWithEmailAndPassword(email,password);
 	}
 	logout(){
 		return this.afAuth.auth.signOut();
 	}
+
 	constructor(private afAuth: AngularFireAuth) {}
 
 }
