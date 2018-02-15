60817dce612a8d3af7cf459d502e4b2505867144
calvinhenry <calvinhenry60@gmail.com>
2018-02-14 22:17:17 -0500

    Added first unit test

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const http = require('http')
 const port = 3000
-
+const assert = require('assert')
 const requestHandler = (request, response) => {
-	console.log(request.url)
-	response.end('Server works')
+	console.log(request.url);
+	//console.log(request)
+	assert.equal(true, false, 'true is true');
+	response.end('Server works');
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
 	if(err){
-		return console.log('something bad happend', err)
+		return console.log('something bad happend', err);
 	}
-	console.log('server is listening on ${port}')
+	console.log('server is listening on ${port}');
 	
 })
