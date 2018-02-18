fb29b76eaa01ba0e2c1287708c635453ce153f21
calvinhenry <calvinhenry60@gmail.com>
2018-02-18 16:00:31 -0500

    Implemented basic facebook request

---
+++ client/src/app/services/facebook-request.ts
@@ -0,0 +1,11 @@
+
+var FB = require('fb'),
+
+FB.api('4', function (res) {
+  if(!res || res.error) {
+   console.log(!res ? 'error occurred' : res.error);
+   return;
+  }
+  console.log(res.id);
+  console.log(res.name);
+});
