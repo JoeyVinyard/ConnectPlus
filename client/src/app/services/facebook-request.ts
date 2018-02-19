3303c2db0918930825f7b31e30f33d8cc73cf5f4
Calvin Henry <calvinhenry60@gmail.com>
2018-02-18 21:34:14 -0500

    added request for friendlist implementation

--- client/src/app/services/facebook-request.ts
+++ client/src/app/services/facebook-request.ts
@@ -1,6 +1,6 @@
 
 var FB = require('fb'),
-
+id = '146089319399243';
 FB.api('4', function (res) {
   if(!res || res.error) {
    console.log(!res ? 'error occurred' : res.error);
@@ -8,4 +8,15 @@ FB.api('4', function (res) {
   }
   console.log(res.id);
   console.log(res.name);
+});
+
+
+FB.api('4', { fields: [id, 'Calvin Henry'] }, function (res) {
+  if(!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+  console.log(res.id);
+  console.log(res.name);
+  console.log(res);
 });
