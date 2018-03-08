f885edcbdacbc2c40f72b306c56f17cb3f62eb60
JoeyVinyard <vinyardjoseph@gmail.com>
2018-03-08 16:10:00 -0500

    Finalized client services for basic profile data

--- client/src/app/services/database.service.ts
+++ client/src/app/services/database.service.ts
@@ -13,6 +13,7 @@ export class DatabaseService {
 		})
 	};
 
+	//Create user profile in firebase based on the User object. Returns a promise to the snapshot of the data posted, or an error message
 	createUser(user: User): Promise<any>{
 		return new Promise((resolve, reject) => {
 			this.http.post("http://localhost:3000/createUser", JSON.stringify(user), this.httpOptions).subscribe((data) => {
@@ -23,6 +24,7 @@ export class DatabaseService {
 			});
 		})
 	}
+	//Update user profile in firebase based on the User object. Returns promise to the snapshot of the data posted, or an error message
 	updateUser(user: User): Promise<any>{
 		return new Promise((resolve, reject) => {
 			this.http.post("http://localhost:3000/updateUser", JSON.stringify(user), this.httpOptions).subscribe((data) => {
@@ -33,10 +35,10 @@ export class DatabaseService {
 			});
 		})
 	}
+	//Update user profile in firebase based on the User object. Returns a promise to the snapshot of the data that was deleted, or an error message
 	deleteUser(user: User): Promise<any>{
 		return new Promise((resolve, reject) => {
 			this.http.delete("http://localhost:3000/deleteUser/"+user.uid, this.httpOptions).subscribe((data) => {
-				console.log(data);
 				if(data)
 					resolve(data["payload"]);
 				else
@@ -44,16 +46,38 @@ export class DatabaseService {
 			});
 		})
 	}
+	//Returns a promise to specific user based on the uid provided, or an error message.
 	getUser(uid: String): Promise<any>{
 		return new Promise((resolve, reject) => {
 			this.http.get("http://localhost:3000/getUser/"+uid, this.httpOptions).subscribe((data) => {
-				console.log(data);
 				if(data)
 					resolve(data["payload"]);
 				else
 					reject(data["err"]);
 			});
 		})
+	}
+	//Returns a promise to a list of users based on the uids provided, or an error message.
+	getMultipleUsers(uids: String[]): Promise<any>{
+		return new Promise((resolve, reject) => {
+			this.http.get("http://localhost:3000/getUsers/"+uids.join("&"), this.httpOptions).subscribe((data) => {
+				if(data)
+					resolve(data["payload"]);
+				else
+					reject(data["err"]);
+			})
+		});
+	}
+	//Returns a promsie to a list of all users, or an error message.
+	getAllUsers(): Promise<any>{
+		return new Promise((resolve, reject) => {
+			this.http.get("http://localhost:3000/getAllUsers", this.httpOptions).subscribe((data) => {
+				if(data)
+					resolve(data["payload"]);
+				else
+					reject(data["err"]);
+			})
+		});
 	}
 
 	constructor(private http: HttpClient) {}
