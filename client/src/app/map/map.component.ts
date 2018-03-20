c9cacee716fb3f61900f53fea4c5e9c0b3fd0ade
JoeyVinyard <vinyardjoseph@gmail.com>
2018-03-19 22:16:36 -0400

    Map now gets nearby users from the database. Also fixed server not listening for all kinds of methods

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -7,122 +7,117 @@ import { DatabaseService } from '../services/database.service';
 import { LocationService } from '../services/location.service';
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+	selector: 'app-map',
+	templateUrl: './map.component.html',
+	styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
 	lat: number = 40.678418;
 	lng: number = -76.809007;
 
-  editMood = false;
-  editRange = false;
+	editMood = false;
+	editRange = false;
 
-  toggleMood(){
-    this.editMood = !this.editMood;
-  }
+	nearbyUsers = [];
 
-  toggleRange(){
-    this.editRange = !this.editRange;
-  }
+	toggleMood(){
+		this.editMood = !this.editMood;
+	}
 
-  model = {
-    user: new User(),
-    moodStatus: ""
-  }
-  errors = {
-    mood: ""
-  }
+	toggleRange(){
+		this.editRange = !this.editRange;
+	}
 
-  MoodStatus = "Mood Status";
+	model = {
+		user: new User(),
+		moodStatus: ""
+	}
+	errors = {
+		mood: ""
+	}
 
-  moodChange(){
-      console.log(this.model);
-      this.model.user.moodStatus = this.model.moodStatus;
-    this.auth.getUser().then((user) => {
-      //this.model.user.uid = user.uid;
-      this.db.updateUser(this.model.user).then((data) => {
-        console.log(data);
-        // console.log(user.data.moodStatus);
+	MoodStatus = "Mood Status";
 
-        this.errors.mood = "Your mood status has been updated!"
-        //this.router.navigateByUrl('map');
-      }).catch((err)=>{
-        console.error(err);
-        this.errors.mood = "Your mood status has NOT been updated!"
+	moodChange(){
+		console.log(this.model);
+		this.model.user.moodStatus = this.model.moodStatus;
+		this.auth.getUser().then((user) => {
+			this.db.updateUser(this.model.user).then((data) => {
+				console.log(data);
+				this.errors.mood = "Your mood status has been updated!"
+			}).catch((err)=>{
+				console.error(err);
+				this.errors.mood = "Your mood status has NOT been updated!"
 
-        //Form rejected for some reason
-      })
-    });
+				//Form rejected for some reason
+			})
+		});
 
 
-  }
+	}
 
-  //ZOOM VALUE FOR MAP
-  zoom: number = 15;
-  currentZoom: number = 15;
+	//ZOOM VALUE FOR MAP
+	zoom: number = 15;
+	currentZoom: number = 15;
 
-  zoomMap(){
-    this.zoom = this.currentZoom;
-    
-  }
+	zoomMap(){
+		this.zoom = this.currentZoom;
+		
+	}
 
-  userVisible = false;
+	userVisible = false;
 
-  viewUser(){
-    this.userVisible = true;
-    console.log("Clicked");
-  }
+	viewUser(){
+		this.userVisible = true;
+		console.log("Clicked");
+	}
 
-  closeUser(){
-    this.userVisible = false;
-  }
-
-
-  particlesConfig;
-  submitted = false;
+	closeUser(){
+		this.userVisible = false;
+	}
 
 
-  constructor(private auth: AuthService, public pConfig: ParticlesConfigService, private router: Router, private db: DatabaseService, public loc: LocationService ) {
+	particlesConfig;
+	submitted = false;
 
-    loc.getLocation().then((l)=> {
-      auth.getUser().then((u) => {
-        db.storeLocation(l, u.uid).then((d) =>{
-          console.log(d);
-          this.lat = l.latitude;
-          this.lng = l.longitude;
-        }).catch((e) =>{
-          console.error(e);
-        })
-      })
+
+	constructor(private auth: AuthService, public pConfig: ParticlesConfigService, private router: Router, private db: DatabaseService, public loc: LocationService ) {
+
+		loc.getLocation().then((l)=> {
+			auth.getUser().then((u) => {
+				db.storeLocation(l, u.uid).then((d) =>{
+					this.lat = l.latitude;
+					this.lng = l.longitude;
+					db.getNearbyUsers(u.uid).then((nearbyUsers) => {
+						console.log("Nearby:",nearbyUsers);
+						this.nearbyUsers = nearbyUsers;
+					}).catch((err) => {
+						console.error(err);
+					})
+				}).catch((e) =>{
+					console.error(e);
+				})
+			})
 		})
+		this.auth.isAuthed().then((user) => {
+			console.log("Authed:",user)
+			this.model.user.uid = user.uid;
+		});  
 
-    this.auth.isAuthed().then((user) => {
-      console.log("Authed:",user)
-      this.model.user.uid = user.uid;
-    });  
+		this.auth.getUser().then((user) => {
+			this.model.user.uid = user.uid;
+			this.db.getUser(user.uid).then((userData) => {
+				this.model.moodStatus = userData.moodStatus;
 
-    this.auth.getUser().then((user) => {
-      this.model.user.uid = user.uid;
-      this.db.getUser(user.uid).then((userData) => {
-         this.model.moodStatus = userData.moodStatus;
-
-        this.model.user = userData;
-        console.log(userData)
-      })
-     // this.model.user.fullName = this.model.user.firstName + ' ' + this.model.user.lastName;
-    //  let fullname = this.model.user.firstName + ' ' + this.model.user.lastName;
-     // this.model.user.fullName = this.model.user.firstName;
+				this.model.user = userData;
+				console.log(userData)
+			})
+		});
 
 
+	}
 
-
-    });
-
-
-  }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
