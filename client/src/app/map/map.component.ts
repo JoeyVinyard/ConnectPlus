7f5803d6f958e6e65e6fb05c84d5e304b5a0317f
Cameron Glass <glass10@purdue.edu>
2018-03-19 22:42:56 -0400

    Fix to map.ts

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -15,104 +15,6 @@ export class MapComponent implements OnInit {
 	lat: number = this.lat;
 	lng: number = this.lng;
 
-  editMood = false;
-  editRange = false;
-
-  toggleMood(){
-    this.editMood = !this.editMood;
-  }
-
-  toggleRange(){
-    this.editRange = !this.editRange;
-  }
-
-  model = {
-    user: new User(),
-    moodStatus: ""
-  }
-  errors = {
-    mood: ""
-  }
-
-  MoodStatus = "Mood Status";
-
-  moodChange(){
-      console.log(this.model);
-      this.model.user.moodStatus = this.model.moodStatus;
-    this.auth.getUser().then((user) => {
-      //this.model.user.uid = user.uid;
-      this.db.updateUser(this.model.user).then((data) => {
-        console.log(data);
-        // console.log(user.data.moodStatus);
-
-        this.errors.mood = "Your mood status has been updated!"
-        //this.router.navigateByUrl('map');
-      }).catch((err)=>{
-        console.error(err);
-        this.errors.mood = "Your mood status has NOT been updated!"
-
-        //Form rejected for some reason
-      })
-    });
-
-
-  }
-
-  //ZOOM VALUE FOR MAP
-  zoom: number = 15;
-  currentZoom: number = 15;
-
-  zoomMap(){
-    this.zoom = this.currentZoom;
-    
-  }
-
-  userVisible = false;
-
-  viewUser(){
-    this.userVisible = true;
-    console.log("Clicked");
-  }
-
-  closeUser(){
-    this.userVisible = false;
-  }
-
-
-
-  filterVisible = false;
-
-  viewFilter(){
-    this.filterVisible = true;
-  }
-
-  closeFilter(){
-    this.filterVisible = false;
-  }
-
-  toggleFilter(){
-    this.filterVisible = !this.filterVisible;
-    console.log("hit");
-  }
-
-
-
-  particlesConfig;
-  submitted = false;
-
-
-  constructor(private auth: AuthService, public pConfig: ParticlesConfigService, private router: Router, private db: DatabaseService, public loc: LocationService ) {
-
-    loc.getLocation().then((l)=> {
-      auth.getUser().then((u) => {
-        db.storeLocation(l, u.uid).then((d) =>{
-          console.log(d);
-          this.lat = l.latitude;
-          this.lng = l.longitude;
-        }).catch((e) =>{
-          console.error(e);
-        })
-      })
 	editMood = false;
 	editRange = false;
 
@@ -172,7 +74,22 @@ export class MapComponent implements OnInit {
 
 	closeUser(){
 		this.userVisible = false;
-	}
+  }
+  
+  filterVisible = false;
+
+  viewFilter(){
+    this.filterVisible = true;
+  }
+
+  closeFilter(){
+    this.filterVisible = false;
+  }
+
+  toggleFilter(){
+    this.filterVisible = !this.filterVisible;
+    console.log("hit");
+  }
 
 
 	particlesConfig;
