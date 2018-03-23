2b5f86b82a54cb57c56e1cf9ceb3f980661f6004
Victoria Oldson <voldson@purdue.edu>
2018-03-23 13:13:18 -0400

    filter by interests

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -173,7 +173,7 @@ export class MapComponent implements OnInit {
 				console.log(data);
 				
 				if(this.model.user.filterSports){
-
+					this.filterUsersBasedOnSports();
 				}
 				else{
 					this.maintainFilter();
@@ -194,7 +194,7 @@ export class MapComponent implements OnInit {
 						console.log(data);
 
 						if(this.model.user.filterMusic){
-
+							this.filterUsersBasedOnMusic();
 						}
 						else{
 							this.maintainFilter();
@@ -213,7 +213,7 @@ export class MapComponent implements OnInit {
 						console.log(data);
 
 						if(this.model.user.filterFood){
-
+							this.filterUsersBasedOnFood();
 						}
 						else{
 							this.maintainFilter();
@@ -260,9 +260,7 @@ export class MapComponent implements OnInit {
 
 					}).catch((err)=>{
 						console.error(err);
-
 					})
-
 		});
 	}
 	linkedinFilter(){
@@ -308,15 +306,15 @@ export class MapComponent implements OnInit {
 		this.filteredUsers = this.nearbyUsers;
 		var count = 0;
 		if(this.model.user.filterSports){
-
+			this.filterUsersBasedOnSports();
 			count++;
 		}
 		if(this.model.user.filterMusic){
-
+			this.filterUsersBasedOnMusic();
 			count++;
 		}
 		if(this.model.user.filterFood){
-
+			this.filterUsersBasedOnFood();
 			count++;
 		}
 		if(this.model.user.filterFacebook){
@@ -340,10 +338,6 @@ export class MapComponent implements OnInit {
 			this.filteredUsers = this.nearbyUsers;
 		}
 	}
-
-
-
-
 
   particlesConfig;
   submitted = false;
@@ -421,6 +415,153 @@ export class MapComponent implements OnInit {
   ngOnInit() {
   }
 
+  filterUsersBasedOnSports(){
+  	var filterUsersArray = [];
+  	if(true /*check facebook thing*/){
+  		var p = new Promise((resolve, reject) => {
+  			this.filteredUsers.forEach((user) => {
+  				var match = false;
+ 				if(user.sports1 == true && this.model.user.sports1 == true){
+					match = true;
+				}
+				if(user.sports2 == true && this.model.user.sports2 == true){
+					match = true;
+				}
+				if(user.sports3 == true && this.model.user.sports3 == true){
+					match = true;
+				}
+				if(user.sporst4 == true && this.model.user.sports4 == true){
+					match = true;
+				}
+				if(user.sports5 == true && this.model.user.sports5 == true){
+					match = true;
+				}
+				if(user.sports6 == true && this.model.user.sports6 == true){
+					match = true;
+				}
+				if(user.sports7 == true && this.model.user.sports7 == true){
+					match = true;
+				}
+				if(user.sports8 == true && this.model.user.sports8 == true){
+					match = true;
+				}
+				if(user.sports9 == true && this.model.user.sports9 == true){
+					match = true;
+				}
+				if(user.sports10 == true && this.model.user.sports10 == true){
+					match = true;
+				}
+
+  				if(match){
+					filterUsersArray.push(user);
+  				}									
+  				resolve(filterUsersArray);
+  			})
+  		}).then((users: any) => {
+  				this.filteredUsers = filterUsersArray;
+  				console.log("Filtered Users:", filterUsersArray);
+  		});
+  	}
+  }
+
+  filterUsersBasedOnMusic(){
+  	  	var filterUsersArray = [];
+  	if(true /*check facebook thing*/){
+  		var p = new Promise((resolve, reject) => {
+  			this.filteredUsers.forEach((user) => {
+  				var match = false;
+ 				if(user.music1 == true && this.model.user.music1 == true){
+					match = true;
+				}
+				if(user.music2 == true && this.model.user.music2 == true){
+					match = true;
+				}
+				if(user.music3 == true && this.model.user.music3 == true){
+					match = true;
+				}
+				if(user.music4 == true && this.model.user.music4 == true){
+					match = true;
+				}
+				if(user.music5 == true && this.model.user.music5 == true){
+					match = true;
+				}
+				if(user.music6 == true && this.model.user.music6 == true){
+					match = true;
+				}
+				if(user.music7 == true && this.model.user.music7 == true){
+					match = true;
+				}
+				if(user.music8 == true && this.model.user.music8 == true){
+					match = true;
+				}
+				if(user.music9 == true && this.model.user.music9 == true){
+					match = true;
+				}
+				if(user.music10 == true && this.model.user.music10 == true){
+					match = true;
+				}
+
+  				if(match){
+					filterUsersArray.push(user);
+  				}									
+  				resolve(filterUsersArray);
+  			})
+  		}).then((users: any) => {
+  				this.filteredUsers = filterUsersArray;
+  				console.log("Filtered Users:", filterUsersArray);
+  		});
+  	}
+  }
+
+  filterUsersBasedOnFood(){
+  	var filterUsersArray = [];
+  	if(true /*check facebook thing*/){
+  		var p = new Promise((resolve, reject) => {
+  			this.filteredUsers.forEach((user) => {
+  				var match = false;
+ 				if(user.food1 == true && this.model.user.food1 == true){
+					match = true;
+				}
+				if(user.food2 == true && this.model.user.food2 == true){
+					match = true;
+				}
+				if(user.food3 == true && this.model.user.food3 == true){
+					match = true;
+				}
+				if(user.food4 == true && this.model.user.food4 == true){
+					match = true;
+				}
+				if(user.food5 == true && this.model.user.food5 == true){
+					match = true;
+				}
+				if(user.food6 == true && this.model.user.food6 == true){
+					match = true;
+				}
+				if(user.food7 == true && this.model.user.food7 == true){
+					match = true;
+				}
+				if(user.food8 == true && this.model.user.food8 == true){
+					match = true;
+				}
+				if(user.food9 == true && this.model.user.food9 == true){
+					match = true;
+				}
+				if(user.food10 == true && this.model.user.food10 == true){
+					match = true;
+				}
+
+  				if(match){
+					filterUsersArray.push(user);
+  				}									
+  				resolve(filterUsersArray);
+  			})
+  		}).then((users: any) => {
+  				this.filteredUsers = filterUsersArray;
+  				console.log("Filtered Users:", filterUsersArray);
+  		});
+  	}
+  }
+
   filterUsersBasedOnFacebook(){
   	var filterUsersArray = [];
   	if(true /*check facebook thing*/){
@@ -459,9 +600,7 @@ export class MapComponent implements OnInit {
   		}).catch((err) => {
   			console.error(err);
   		});
-
   	}
-
   }
 
   filterUsersBasedOnTwitter(){
@@ -477,10 +616,8 @@ export class MapComponent implements OnInit {
   				this.filteredUsers.forEach((user) => {
   					this.db.getTwitterFollowees(user.uid).then((nearbyFollowee) => {
   						var match = false;
-
   						nearbyFollowee.forEach((followee) => {
 								if(followeeMap.get(followee)){
-									
 									match = true;
 								}
 							});
