3eab113b90cefcfe5b672528d11f1db401c0ae34
Joey Vinyard <jviny98@yahoo.com>
2018-04-11 11:47:20 -0400

    Heroku deploy

---
+++ server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	fbConfig:{
+		apiKey: 'AIzaSyCMeUYzNMhFjl8pnSO2mRe8wgRpYYR1E5w',
+		authDomain: 'connecplus-c6ddf.firebaseapp.com',
+		databaseURL: 'https://connecplus-c6ddf.firebaseio.com/',
+		projectId: 'connecplus-c6ddf',
+		storageBucket: 'connecplus-c6ddf.appspot.com',
+	},
+	password: "adminaccount",
+	youtubeAPIKey: "AIzaSyAYPFjyBz7atRsbr5GyJtlRiBLpu6hcD0A"
+}
+
+//Youtube secret: GRiuj_y3NBtc1HlooLqMbCF4
+//Youtube client-id: 374666659146-c9n74gdloum89050ckabsfssh0oe4qkl.apps.googleusercontent.com
