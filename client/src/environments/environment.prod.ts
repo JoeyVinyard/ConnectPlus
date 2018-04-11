c1c7ec8dddb9caf175ffda5385614273ce64f2b6
Joey Vinyard <jviny98@yahoo.com>
2018-04-11 11:58:28 -0400

    Fixed port

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: "localhost:3000/"
+  serverUrl: "http://cplusserver.herokuapp.com/"
 };
--- server/index.js
+++ server/index.js
@@ -1,2 +1,2 @@
 var server = require("./server");
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
