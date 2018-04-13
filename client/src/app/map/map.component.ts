28b4ce95db93bfa4f309904d6df17401f2b3b9fc
Cameron Glass <glass10@purdue.edu>
2018-04-13 00:53:39 -0400

    Large update to commonalities system. Still not working

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -57,6 +57,7 @@ export class MapComponent implements OnInit {
 	commonMap = new Map();
 	CommonUsersList = [];
 	CommonUsersListtemp = [];
+
 	temp;
 	holder;
 
@@ -627,150 +628,228 @@ export class MapComponent implements OnInit {
 	}
 
 	filterUsersBasedOnFacebook(num: number) {
+		return new Promise((mainResolve, mainReject) => {
+			var filterUsersArray = [];
+			if (true /*check facebook thing*/) {
 
-		var filterUsersArray = [];
-		if (true /*check facebook thing*/) {
+				this.db.getFacebookFriends(this.model.user.uid).then((friends) => {
+					var friendMap = new Map();
 
-			this.db.getFacebookFriends(this.model.user.uid).then((friends) => {
-				var friendMap = new Map();
+					friends.forEach((friend) => {
+						friendMap.set(friend, 1);
 
-				friends.forEach((friend) => {
-					friendMap.set(friend, 1);
-
-				});
-				var p = new Promise((resolve, reject) => {
-					this.filteredUsers.forEach((user) => {
-						//this.facebookCommon = 0;
-						//this.holder = this.commonMap.get(user.uid);
-						this.db.getFacebookFriends(user.uid).then((nearbyFriend) => {
-							var match = false;
-							this.holder = this.commonMap.get(user.uid);
-							//console.log(this.holder);
-							nearbyFriend.forEach((friend) => {
-								//console.log(friend);
-								if (friendMap.get(friend)) {
-									match = true;
-									this.facebookCommon = this.facebookCommon + 1;
-									//console.log(this.facebookCommon)
-									// this.holder.facebook = true;
-									// this.holder.FB = "Facebook"
-									(this.commonMap.get(user.uid)).facebook = true;
-									(this.commonMap.get(user.uid)).FB = "Facebook"
-								}
-							});
-							// this.holder.facebookNum = this.facebookCommon;
-							(this.commonMap.get(user.uid)).facebookNum = this.facebookCommon;
-							if (match) {
-
-								filterUsersArray.push(user);
-							}
-							resolve(filterUsersArray);
-
-						}).catch((err) => {
-							console.log(err);
-							reject(err);
-						});
-						//this.holder.facebookNum = 0;
-						//this.holder.facebookNum = (this.facebookCommon /2);
 					});
-				}).then((users: any) => {
-					if (!num) {
-						this.filteredUsers = filterUsersArray;
-						console.log("Filtered Users Facebook:", filterUsersArray);
-					}
-					else {
+					var p = new Promise((resolve, reject) => {
+						this.filteredUsers.forEach((user) => {
+							//reset all commonality values
+							this.facebookCommon = 0;
+							this.holder = this.commonMap.get(user.uid);
+							(this.commonMap.get(user.uid)).FB = "Facebook";
+							(this.commonMap.get(user.uid)).facebookNum = 0;
 
-					}
+							this.db.getFacebookFriends(user.uid).then((nearbyFriend) => {
+								var match = false;
+
+								nearbyFriend.forEach((friend) => {
+									//console.log(friend);
+									if (friendMap.get(friend)) {
+										match = true;
+										this.facebookCommon = this.facebookCommon + 1;
+										
+										(this.commonMap.get(user.uid)).facebook = true;
+										(this.commonMap.get(user.uid)).FB = "Facebook";
+									}
+								});
+								// this.holder.facebookNum = this.facebookCommon;
+								
+								if (match) {
+									(this.commonMap.get(user.uid)).facebookNum = this.facebookCommon;
+									filterUsersArray.push(user);
+								}
+								resolve(filterUsersArray);
+
+							}).catch((err) => {
+								console.log(err);
+								reject(err);
+							});
+							//this.holder.facebookNum = 0;
+							//this.holder.facebookNum = (this.facebookCommon /2);
+						});
+					}).then((users: any) => {
+						if (!num) {
+							this.filteredUsers = filterUsersArray;
+							console.log("Filtered Users Facebook:", filterUsersArray);
+						}
+						else {
+							console.log("Facebook Filtering Done")
+							
+						}
+						mainResolve("Facebook")
+					});
+				}).catch((err) => {
+					console.error(err);
 				});
-			}).catch((err) => {
-				console.error(err);
-			});
-		}
+			}
+		});
 	}
 
 	filterUsersBasedOnTwitter(num: number) {
+		return new Promise((mainResolve, mainReject) => {
+			var filterUsersArray = [];
+			if (true) {
+				this.db.getTwitterFollowees(this.model.user.uid).then((followees) => {
+					var followeeMap = new Map();
 
-		var filterUsersArray = [];
-		if (true) {
-			this.db.getTwitterFollowees(this.model.user.uid).then((followees) => {
-				var followeeMap = new Map();
-
-				followees.forEach((followee) => {
-					followeeMap.set(followee, 1);
-				});
-				var p = new Promise((resolve, reject) => {
-					this.filteredUsers.forEach((user) => {
-						//this.twitterCommon = 0;
-						this.db.getTwitterFollowees(user.uid).then((nearbyFollowee) => {
-							var match = false;
-							this.holder = this.commonMap.get(user.uid);
-
-							nearbyFollowee.forEach((followee) => {
-								if (followeeMap.get(followee)) {
-									match = true;
-									this.twitterCommon = this.twitterCommon + 1;
-									this.holder.twitter = true;
-									this.holder.TW = "Twitter"
-								}
-							});
-							this.holder.twitterNum = this.twitterCommon;
-
-							if (match) {
-								filterUsersArray.push(user);
-							}
-							resolve(filterUsersArray);
-						}).catch((err) => {
-							console.log(err);
-							reject(err);
-						});
+					followees.forEach((followee) => {
+						followeeMap.set(followee, 1);
 					});
-				}).then((users: any) => {
-					if (!num) {
-						this.filteredUsers = filterUsersArray;
-						console.log("Filtered Users:", filterUsersArray);
-					}
-					else {
+					var p = new Promise((resolve, reject) => {
+						this.filteredUsers.forEach((user) => {
+							this.twitterCommon = 0;
+							this.holder = this.commonMap.get(user.uid);
+							(this.commonMap.get(user.uid)).TW = "Twitter";
 
-					}
+							this.db.getTwitterFollowees(user.uid).then((nearbyFollowee) => {
+								var match = false;
+
+								nearbyFollowee.forEach((followee) => {
+									if (followeeMap.get(followee)) {
+										match = true;
+										this.twitterCommon = this.twitterCommon + 1;
+										(this.commonMap.get(user.uid)).twitter = true;
+										(this.commonMap.get(user.uid)).TW = "Twitter";
+									}
+								});
+								(this.commonMap.get(user.uid)).twitterNum = this.twitterCommon;
+
+								if (match) {
+									filterUsersArray.push(user);
+								}
+								resolve(filterUsersArray);
+							}).catch((err) => {
+								console.log(err);
+								reject(err);
+							});
+						});
+					}).then((users: any) => {
+						if (!num) {
+							this.filteredUsers = filterUsersArray;
+							console.log("Filtered Users:", filterUsersArray);
+						}
+						else {
+							console.log("Twitter Filtering Done")
+							
+						}
+						mainResolve("Twitter")
+					});
+				}).catch((err) => {
+					console.error(err);
 				});
-			}).catch((err) => {
-				console.error(err);
-			});
-		}
+			}
+		})
 	}
 
 	filterUsersBasedOnYoutube(num: number) {
+		return new Promise((mainResolve, mainReject) => {
+			var filterUsersArray = [];
+			if (true) {
+				this.db.getYoutubeSubscribers(this.model.user.uid).then((subscribers) => {
+					var subscriberMap = new Map();
+					console.log("this is the subs", subscribers)
+					Object.keys(subscribers).forEach((subscriber) => {
+						subscriberMap.set(subscriber, 1);
+					});
 
-		var filterUsersArray = [];
-		if (true) {
-			this.db.getYoutubeSubscribers(this.model.user.uid).then((subscribers) => {
-				var subscriberMap = new Map();
-				//console.log("this is the subs", subscribers)
-				Object.keys(subscribers).forEach((subscriber) => {
-					subscriberMap.set(subscriber, 1);
-				});
-				var p = new Promise((resolve, reject) => {
-					//console.log("youtube map works?", subscriberMap)
-					this.filteredUsers.forEach((user) => {
-						this.youtubeCommon = 0;
-						this.db.getYoutubeSubscribers(user.uid).then((nearbySubscriber) => {
-							var match = false;
+					var p = new Promise((resolve, reject) => {
+						console.log("In Youtube Promise")
+						this.filteredUsers.forEach((user) => {
+							this.youtubeCommon = 0;
 							this.holder = this.commonMap.get(user.uid);
-							Object.keys(nearbySubscriber).forEach((subscriber) => {
+							(this.commonMap.get(user.uid)).YT = "Youtube";
 
-								//console.log(subscriber)
-								//console.log(subscriberMap);
-								if (subscriberMap.get(subscriber)) {
-									match = true;
-									//console.log("hellllllllllooooooooooo")
-									this.youtubeCommon = this.youtubeCommon + 1;
-									this.holder.youtube = true;
-									this.holder.YT = "Youtube"
+							this.db.getYoutubeSubscribers(user.uid).then((nearbySubscriber) => {
+								var match = false;
+								console.log("Current User Info: " + nearbySubscriber)
+								if(nearbySubscriber != null){
+									Object.keys(nearbySubscriber).forEach((subscriber) => {
+
+										//console.log(subscriber)
+										//console.log(subscriberMap);
+										if (subscriberMap.get(subscriber)) {
+											match = true;
+											//console.log("hellllllllllooooooooooo")
+											this.youtubeCommon = this.youtubeCommon + 1;
+											(this.commonMap.get(user.uid)).youtube = true;
+											(this.commonMap.get(user.uid)).YT = "Youtube";
+										}
+									});
 								}
-							});
-							this.holder.youtubeNum = this.youtubeCommon;
+								
+								(this.commonMap.get(user.uid)).youtubeNum = this.youtubeCommon;
 
+								if (match) {
+									filterUsersArray.push(user);
+								}
+								resolve(filterUsersArray);
+							}).catch((err) => {
+								console.log(err);
+								reject(err);
+							});
+						});
+					}).then((users: any) => {
+						if (!num) {
+							this.filteredUsers = filterUsersArray;
+							console.log("Filtered Users:", filterUsersArray);
+						}
+						else {
+							console.log("Youtube Filtering Done")
+							
+						}
+						mainResolve("Youtube")
+					}).catch((err) => {
+						console.error(err);
+					});
+				}).catch((err) => {
+					console.error(err);
+				});
+			}
+		});
+	}
+
+	filterUsersBasedOnBlackboard(num: number) {
+
+		return new Promise((mainResolve, mainReject) => {
+			//console.log("Blackboard");
+			var filterUsersArray = [];
+			this.db.getClasses(this.model.user.uid).then((classes) => {
+				var classesMap = new Map();
+
+				classes.forEach((singleClass) => {
+					classesMap.set(singleClass, 1);
+				});
+
+				var p = new Promise((resolve, reject) => {
+					this.filteredUsers.forEach((user) => {
+						this.blackboardCommon = 0;
+						(this.commonMap.get(user.uid)).blackboardNum = 0;
+
+						this.db.getClasses(user.uid).then((nearbyUser) => {
+							var match = false;
+							
+
+							if (nearbyUser != null) {
+								nearbyUser.forEach((singleClass) => {
+									if (classesMap.get(singleClass)) {
+										match = true;
+										this.blackboardCommon = this.blackboardCommon + 1;
+										(this.commonMap.get(user.uid)).blackboard = true;
+										(this.commonMap.get(user.uid)).BB = "Blackboard";
+									}
+								});
+
+							}
 							if (match) {
+								(this.commonMap.get(user.uid)).blackboardNum = this.blackboardCommon;
 								filterUsersArray.push(user);
 							}
 							resolve(filterUsersArray);
@@ -785,71 +864,17 @@ export class MapComponent implements OnInit {
 						console.log("Filtered Users:", filterUsersArray);
 					}
 					else {
-
+						console.log("Blackboard Filtering Done")
 					}
+					mainResolve("Blackboard")
 				}).catch((err) => {
 					console.error(err);
 				});
 			}).catch((err) => {
-				console.error(err);
-			});
-		}
-	}
+				console.log(err);
 
-	filterUsersBasedOnBlackboard(num: number) {
-		//console.log("Blackboard");
-		var filterUsersArray = [];
-		this.db.getClasses(this.model.user.uid).then((classes) => {
-			var classesMap = new Map();
-
-			classes.forEach((singleClass) => {
-				classesMap.set(singleClass, 1);
-			});
-
-			var p = new Promise((resolve, reject) => {
-				this.filteredUsers.forEach((user) => {
-					this.blackboardCommon = 0;
-
-					this.db.getClasses(user.uid).then((nearbyUser) => {
-						var match = false;
-						this.holder = this.commonMap.get(user.uid);
-
-						if (nearbyUser != null) {
-							nearbyUser.forEach((singleClass) => {
-								if (classesMap.get(singleClass)) {
-									match = true;
-									this.blackboardCommon = this.blackboardCommon + 1;
-									this.holder.blackboard = true;
-									this.holder.BB = "BlackBoard"
-								}
-							});
-							this.holder.blackboardNum = this.blackboardCommon;
-
-						}
-						if (match) {
-							filterUsersArray.push(user);
-						}
-						resolve(filterUsersArray);
-					}).catch((err) => {
-						console.log(err);
-						reject(err);
-					});
-				});
-			}).then((users: any) => {
-				if (!num) {
-					this.filteredUsers = filterUsersArray;
-					console.log("Filtered Users:", filterUsersArray);
-				}
-				else {
-
-				}
-			}).catch((err) => {
-				console.error(err);
-			});
-		}).catch((err) => {
-			console.log(err);
-
-		})
+			})
+		});
 	}
 
 	sendBroadcast() {
@@ -905,6 +930,7 @@ export class MapComponent implements OnInit {
 				//console.log("Nearby:", nearbyUsers);
 				this.nearbyUsers = nearbyUsers;
 				this.CommonUsersList = this.nearbyUsers;
+				
 				nearbyUsers.forEach((nearbyUser) => {
 
 					// this.facebookCommon = 0;
@@ -941,58 +967,62 @@ export class MapComponent implements OnInit {
 		//this.getCommon();		
 	}
 	getCommon() {
-		var p = new Promise((resolve, reject) => {
+		console.log("getCommon Called")
 		this.facebookCommon = 0;
 		this.twitterCommon = 0;
 		this.blackboardCommon = 0;
 		this.youtubeCommon = 0;
-		this.filterUsersBasedOnFacebook(1);
-		this.filterUsersBasedOnTwitter(1);
-		this.filterUsersBasedOnYoutube(1);
-		this.filterUsersBasedOnBlackboard(1);
-			resolve();
-		}).then(() =>{
-			console.log("common", this.commonMap);
+
+		var promises = [];
+		promises.push(this.filterUsersBasedOnFacebook(1));
+		promises.push(this.filterUsersBasedOnTwitter(1));
+		// promises.push(this.filterUsersBasedOnYoutube(1));
+		promises.push(this.filterUsersBasedOnBlackboard(1));
+
+		Promise.all(promises).then(() =>{
+			console.log("common", this.commonMap.get("ZVmOhUAURNOD8t4zqunUdUtjc4B3"));
+			console.log("Promises: " + promises)
 			this.generateTiers();
 		})
-
 	}
 
+
 	generateTiers(){
+		console.log("generateTiers Called")
 		var allTotals = [];
 		var tempTotal = 0;
 		
 		this.commonMap.forEach((user) =>{
-			var p = new Promise((resolve, reject) => {
-				tempTotal = 0;
-				console.log(user);
-				if(user.facebook){
+			tempTotal = 0;
+			console.log(user);
+
+				var flag = true;
+				do{
 					tempTotal += (user.facebookNum * 0.1);
-				}
-				if(user.twitter){
+					// console.log("User got in facebook " + tempTotal)
+
 					tempTotal += (user.twitterNum * 0.1);
-				}
-				if(user.youtube){
-					tempTotal += (user.youtube * 0.1);
-				}
-				if(user.blackboard){
-					tempTotal += user.blackboard;
-				}
-				if(user.interestSub.size != 0){
+					// console.log("User got in twitter " + tempTotal)
+
+					tempTotal += (user.youtubeNum * 0.1);
+					// console.log("User got in youtube " + tempTotal)
+
+					tempTotal += user.blackboardNum;
+					// console.log("User got in blackboard " + tempTotal)
+
 					var intCatNum = user.interestSub.size;
 					var intSubNum = 0;
 					user.interestSub.forEach((interest) =>{
 						intSubNum += interest.value;
 					});
-
 					tempTotal += (intCatNum + intSubNum);
-				}
-				// resolve(tempTotal);
-			}).then(() =>{
-				allTotals.push(tempTotal);
-				console.log(tempTotal);
-			})
+
+					allTotals.push(tempTotal);
+					flag = false;
+				}while(flag)
+			
 		});
+		console.log(allTotals);
 	}
 
 }
